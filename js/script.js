d33bd1b7ec978a3d79f6235b30378bf09f3817fb
Julian <julianm95@protonmail.com>
2019-10-28 11:09:05 -0400

    n

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var printer ["Xerox WorkCenter 3655","Xerox WorkCenter 4250","Xerox WorkCenter 5755","Xerox WorkCenter 5855","Xerox Phaser 4600","Xerox Phaser 4622","Xerox ColorCube 8880","Xerox VersaLink C605X"];
+
+var supplies ["Toner","Maintanence Kit","SmartKit","Waste Bottle","Xerographic Module","Fuser Module","Cyan Toner","Magenta Toner","Yellow Toner","Black Toner","Fuser","Maintanence"]
